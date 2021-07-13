6bdd84effb534bc684b2e0e7771dd6140a28681b
sumansuresh <ssuman9795@gmail.com>
2021-07-13 14:38:27 +0530

    Update AssSample1.java

--- AssSample1.java
+++ AssSample1.java
@@ -1,35 +1,35 @@
 public class AssSample1 {
 
 //AREA OF CRICLE	
-	public void AreaofCircle(int r) 
+	public void areaofCircle(int r) 
 	{
 		double x=3.14*r*r;
 		System.out.println("Area of circle: "+x);
 	}
 	
 //AREA OF TRIANGLE	
-	public void Areaoftriangle(int height,int breadth) 
+	public void areaoftriangle(int height,int breadth) 
 	{
 		int x=(height*breadth)/2;
 		System.out.println("Area of triangle: "+x);
 	}
 	
 //AREA OF SQUARE	
-	public void Areaofsquare(int side) 
+	public void areaofsquare(int side) 
 	{
 		int x=side*side;
 		System.out.println("Area of square: "+x);
 	}
 	
 //AREA OF RECTANGLE	
-	public void Areaofrectangle(int length,int width) 
+	public void areaofrectangle(int length,int width) 
 	{
 		int x=width*length;
 		System.out.println("Area of rectangle: "+x);
 	}
 	
 //AREA OF CUBE	
-	public void AreaofCube(int edge) 
+	public void areaofCube(int edge) 
 	{
 		int x=6*edge*edge;
 		System.out.println("Area of cube: "+x);
@@ -39,11 +39,11 @@ public class AssSample1 {
 	public static void main(String[] args)
 	{
 		AssSample1 obj=new AssSample1();
-		obj.AreaofCircle(5);
-		obj.AreaofCube(4);
-		obj.Areaofrectangle(12, 4);
-		obj.Areaofsquare(8);
-		obj.Areaoftriangle(5, 4);
+		obj.areaofCircle(5);
+		obj.areaofCube(4);
+		obj.areaofrectangle(12, 4);
+		obj.areaofsquare(8);
+		obj.areaoftriangle(5, 4);
 	}
 
 }
